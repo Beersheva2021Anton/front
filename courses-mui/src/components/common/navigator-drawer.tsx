084ca93da39d66014c83fcc1ff0716e2ae832037
Anton Elin <checkano@gmail.com>
2022-01-09 15:50:27 +0200

    Fix drawer

--- courses-mui/src/components/common/navigator-drawer.tsx
+++ courses-mui/src/components/common/navigator-drawer.tsx
@@ -1,31 +1,36 @@
-import { Box, Drawer, IconButton, Tab } from "@mui/material";
+import { Box, Drawer, IconButton, List, ListItem, ListItemText } from "@mui/material";
 import { FC, useState } from "react";
 import { RouteType } from "../../models/common/route-type";
 import { Link } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
+import { fontSize } from "@mui/system";
 
 const NavigatorDrawer: FC<{items: RouteType[]}> = (props) => {
 
     const [visibleDrawer, setVisibleDrawer] = useState(false);
 
-    function showDrawer() {
+    function setDrawer() {
         setVisibleDrawer(!visibleDrawer);
     }
 
     function getLinks() {
         return props.items.map(item => 
-            <Tab key={item.label} component={Link} to={item.path} label={item.label} />)
+            <ListItem key={item.label} component={Link} to={item.path}>
+                <ListItemText disableTypography sx={{ fontSize: '1em' }}>{item.label}</ListItemText>
+            </ListItem>)
     }
 
     return <Box>
-        <IconButton onClick={() => showDrawer()}>
+        <IconButton onClick={() => setDrawer()}>
             <MenuIcon />
         </IconButton>
         <Drawer anchor="left" 
                 open={visibleDrawer} 
-                onClose={() => showDrawer()}
-                onClick={() => showDrawer()}>
-            {getLinks()}
+                onClose={() => setDrawer()}
+                onClick={() => setDrawer()}>
+            <List>
+                {getLinks()}
+            </List>
         </Drawer>
     </Box>
 }
