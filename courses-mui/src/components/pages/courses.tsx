87da87a85a3f289dc7d1247f9cbdfe6e715103fa
Anton Elin <checkano@gmail.com>
2022-01-24 15:01:55 +0200

    Add Details component

--- courses-mui/src/components/pages/courses.tsx
+++ courses-mui/src/components/pages/courses.tsx
@@ -1,24 +1,40 @@
 import { Box, Paper, Typography } from "@mui/material";
-import { FC, useContext, useEffect, useMemo, useState } from "react";
+import { FC, ReactElement, useContext, useEffect, useMemo, useState } from "react";
 import CoursesContext from "../../store/context";
 import { UserData } from "../../models/common/user-data";
 import { DataGrid, GridActionsCellItem, GridRowParams, GridRowsProp } from "@mui/x-data-grid";
 import CourseType from "../../models/course-type";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import InfoIcon from '@mui/icons-material/Info';
 import { college } from "../../config/service-config";
-import ActionConfirmation from "../action-confirmation";
+import ActionConfirmation from "../common/action-confirmation";
+import CourseInfo from "../course-info";
 
 const Courses: FC = () => {
     const context = useContext(CoursesContext);
 
     const [columns, setColumns] = useState(getColumns(context.userData));
     const [confirmOpen, setConfirmOpen] = useState<boolean>(false);
-    const [removeID, setRemoveID] = useState<number>(0);
+    const [infoOpen, setInfoOpen] = useState<boolean>(false);
+    const [courseInfo, setCourseInfo] = useState<CourseType>();
+    const [currentID, setCurrentID] = useState<number>(0);
     const rows = useMemo(() => getRows(context.list), [context.list]);
 
     useEffect(() => {
         setColumns(getColumns(context.userData));
     }, [context.userData]);
+
+    function getRelevantActions(isAdmin: boolean, itemId: number): ReactElement[] {
+        let res: ReactElement[] = [
+            <GridActionsCellItem icon={<InfoIcon/>} label='Details' 
+                onClick={() => showDetails(itemId)} />
+        ]
+        if (isAdmin) {
+            res.push(<GridActionsCellItem icon={<DeleteOutlineIcon />} label='Remove'
+            onClick={() => showRemoveConfirmation(itemId)} />)
+        }
+        return res;
+    }
 
     function getColumns(userData: UserData): any[] {
 
@@ -35,15 +51,10 @@ const Courses: FC = () => {
                 flex: 200, align: 'center', headerAlign: 'center'
             },
             {
-                field: 'actions', type: 'actions', getActions: (params: GridRowParams) => [
-                    <GridActionsCellItem icon={<DeleteOutlineIcon />} label='Remove'
-                        onClick={() => showRemoveConfirmation(params.id as number)} />
-                ]
+                field: 'actions', type: 'actions', getActions: (params: GridRowParams) => 
+                    getRelevantActions(userData.isAdmin, params.id as number)
             }
         ]
-        if (!userData.isAdmin) {
-            res.pop();
-        }
         return res;
     }
 
@@ -51,16 +62,26 @@ const Courses: FC = () => {
         return courses.map(course => course);
     }
 
+    async function showDetails(id: number): Promise<void> {
+        setCurrentID(id);        
+        await college.getCourse(id).then(course => setCourseInfo(course));
+        setInfoOpen(true);
+    }
+
     function showRemoveConfirmation(id: number): void {
-        setRemoveID(id);
+        setCurrentID(id);
         setConfirmOpen(true);
     }
 
     function handleRemove(status: boolean): void {
         if (status) {
-            college.removeCourse(removeID);
+            college.removeCourse(currentID);
         }
         setConfirmOpen(false);
+    }
+
+    function handleInfo(): void {
+        setInfoOpen(false);
     }
 
     return <Box marginX='1em'>
@@ -69,8 +90,9 @@ const Courses: FC = () => {
             <DataGrid rows={rows} columns={columns} />
         </Paper>
         <ActionConfirmation isVisible={confirmOpen} title="Course Remove" 
-            message={`Are you sure you want to remove course with ID '${removeID}'?`} 
+            message={`Are you sure you want to remove course with ID '${currentID}'?`} 
             onClose={handleRemove} />
+        <CourseInfo isVisible={infoOpen} onClose={handleInfo} data={courseInfo!} />
     </Box>
 }
 
